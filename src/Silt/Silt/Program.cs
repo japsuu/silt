a7bb9bc05eb55e44e10702625b305a4564f9bc75
Jasper Honkasalo <jasper.honkasalo@wartsila.com>
2026-01-07 15:02:18 +0200

    Add Serilog logging

--- src/Silt/Silt/Program.cs
+++ src/Silt/Silt/Program.cs
@@ -1,9 +1,37 @@
 ﻿namespace Silt;
 
+using Serilog;
+using Serilog.Events;
+
 internal static class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        SetupLogging();
+
+        try
+        {
+            Log.Information("Starting Silt engine...");
+            Log.Debug("Args: {Args}", args);
+        }
+        catch (Exception ex)
+        {
+            Log.Fatal(ex, "Silt terminated unexpectedly");
+        }
+        finally
+        {
+            Log.CloseAndFlush();
+        }
+    }
+
+
+    private static void SetupLogging()
+    {
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+            .Enrich.FromLogContext()
+            .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}")
+            .CreateLogger();
     }
 }
