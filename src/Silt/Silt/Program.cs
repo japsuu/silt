b51303dff7bb0f064605ce8216e576a46b1ac364
Jasper Honkasalo <jasper.honkasalo@wartsila.com>
2026-01-07 15:24:22 +0200

    Add window creation and input

--- src/Silt/Silt/Program.cs
+++ src/Silt/Silt/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace Silt;
+﻿using Silk.NET.Input;
+using Silk.NET.Maths;
+using Silk.NET.Windowing;
+
+namespace Silt;
 
 using Serilog;
 using Serilog.Events;
 
 internal static class Program
 {
+    private static IWindow _window = null!;
+
+
     private static void Main(string[] args)
     {
         SetupLogging();
@@ -13,6 +20,13 @@ internal static class Program
         {
             Log.Information("Starting Silt engine...");
             Log.Debug("Args: {Args}", args);
+
+            _window = CreateWindow();
+            _window.Load += OnLoad;
+            _window.Update += OnUpdate;
+            _window.Render += OnRender;
+
+            _window.Run();
         }
         catch (Exception ex)
         {
@@ -25,6 +39,34 @@ internal static class Program
     }
 
 
+    private static void OnLoad()
+    {
+        Log.Information("Window loaded");
+        
+        // Setup input
+        IInputContext input = _window.CreateInput();
+        foreach (IKeyboard k in input.Keyboards)
+            k.KeyDown += KeyDown;
+    }
+
+
+    private static void OnUpdate(double deltaTime)
+    {
+    }
+
+
+    private static void OnRender(double deltaTime)
+    {
+    }
+
+
+    private static void KeyDown(IKeyboard keyboard, Key key, int keyCode)
+    {
+        if (key == Key.Escape)
+            _window.Close();
+    }
+
+
     private static void SetupLogging()
     {
         Log.Logger = new LoggerConfiguration()
@@ -33,5 +75,17 @@ internal static class Program
             .Enrich.FromLogContext()
             .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}")
             .CreateLogger();
+    }
+
+
+    private static IWindow CreateWindow()
+    {
+        WindowOptions options = WindowOptions.Default with
+        {
+            Size = new Vector2D<int>(720, 720),
+            Title = "Silt"
+        };
+
+        return Window.Create(options);
     }
 }
